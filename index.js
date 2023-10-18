b26db1625cc7bdf5dc831a349ca8d9e425afbbe7
Pedro Vieira <mvpedro.cc@gmail.com>
2023-10-18 19:31:21 -0300

    Adding new routes

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 
+app.use(express.json());
+
 const pool = new Pool({
-  host: 'db', // this should match the service name in docker-compose
+  host: 'db',
   port: 5432,
   database: 'mydb',
   user: 'myuser',
   password: 'mypassword',
 });
 
-app.get('/', async (req, res) => {
-  const client = await pool.connect();
-  const result = await client.query('SELECT NOW()');
-  const currentTime = result.rows[0].now;
-  client.release();
-  res.send(`Database connected! Current time: ${currentTime}`);
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and email are required." });
+  }
+  try {
+    const result = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id', [name, email]);
+    res.status(201).json({ id: result.rows[0].id });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(3000, () => {
