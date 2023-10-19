4e4f1908e87ace8eae87a7fc903b8ff961cd720c
Pedro Vieira <mvpedro.cc@gmail.com>
2023-10-18 21:21:17 -0300

    Formatting

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
 const app = express();
 
-app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'X-API-Key']
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "X-API-Key"],
+  })
+);
 
 app.use((req, res, next) => {
-  const apiKey = req.get('X-API-Key');
+  const apiKey = req.get("X-API-Key");
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    return res.status(403).json({ error: 'Unauthorized' });
+    return res.status(403).json({ error: "Unauthorized" });
   }
   next();
 });
@@ -21,29 +23,32 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || "db",
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'mydb',
-  user: process.env.DB_USER || 'myuser',
-  password: process.env.DB_PASSWORD || 'mypassword',
+  database: process.env.DB_NAME || "mydb",
+  user: process.env.DB_USER || "myuser",
+  password: process.env.DB_PASSWORD || "mypassword",
 });
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
+    const result = await pool.query("SELECT * FROM users");
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   const { name, email } = req.body;
   if (!name || !email) {
     return res.status(400).json({ error: "Name and email are required." });
   }
   try {
-    const result = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id', [name, email]);
+    const result = await pool.query(
+      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+      [name, email]
+    );
     res.status(201).json({ id: result.rows[0].id });
   } catch (err) {
     res.status(500).json({ error: err.message });
